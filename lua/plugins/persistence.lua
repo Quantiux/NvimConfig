88b0a410e033598fb785ffe2cfeb8433c6682801
Quantiux <roy@quantiux.com>
2024-06-21 12:08:54 -0400

    Added `require("persistence").setup()` in `config()` to make it work

--- lua/plugins/persistence.lua
+++ lua/plugins/persistence.lua
@@ -1,4 +1,14 @@
 local config = function()
+  require("persistence").setup({
+    dir = vim.fn.stdpath("state") .. "/sessions/", -- ~/.local/state/nvim/sessions
+    options = { "buffers", "curdir", "tabpages", "winsize" }, -- options for `:mksession`
+    pre_save = nil, -- a function to call before saving the session
+    post_save = nil, -- a function to call after saving the session
+    save_empty = false, -- don't save if there are no open file buffers
+    pre_load = nil, -- a function to call before loading the session
+    post_load = nil, -- a function to call after loading the session
+  })
+
 	-- which-key mapping
 	local wk = require("which-key")
 	wk.register({
@@ -7,7 +17,7 @@ local config = function()
 			"Restore last session for cwd",
 		},
 		["<leader>pl"] = {
-			":lua require('persistence').load({ last = true })<CCR>",
+			":lua require('persistence').load({ last = true })<CR>",
 			"Restore last session",
 		},
 		["<leader>pq"] = { ":lua require('persistence').stop()<CR>", "Stop without saving session" },
@@ -17,6 +27,5 @@ end
 return {
 	"folke/persistence.nvim",
 	event = "BufReadPre",
-	opts = {},
 	config = config,
 }
