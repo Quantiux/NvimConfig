f8e80e867e28f60eed71c5e90a337a29ee220433
Quantiux <roy@quantiux.com>
2024-06-30 16:03:29 -0400

    Added showmode, whichwrap

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -4,25 +4,26 @@ local opt = vim.opt
 opt.tabstop = 2
 opt.shiftwidth = 2
 opt.softtabstop = 2
-opt.expandtab = true                                    -- convert tabs to spaces
+opt.expandtab = true -- convert tabs to spaces
 opt.smartindent = true
-opt.wrap = false
+opt.wrap = true
 
 -- Search
 opt.incsearch = true
-opt.ignorecase = true                                   -- ignore case in search pattern
+opt.ignorecase = true -- ignore case in search pattern
 opt.smartcase = true
 opt.hlsearch = true
 
 -- Appearance
 opt.number = true
 opt.relativenumber = true
-opt.termguicolors = true                                -- most terms support this
+opt.termguicolors = true -- most terms support this
 opt.colorcolumn = "100"
 opt.signcolumn = "yes"
-opt.cmdheight = 0                                       -- disable commandline (below statusline)
+opt.cmdheight = 0 -- disable commandline (below statusline)
 opt.scrolloff = 10
 opt.completeopt = "menuone,noinsert,noselect"
+opt.showmode = false -- disable mode -- INSERT -- etc below statusline
 
 -- Behavior
 opt.hidden = true
@@ -37,7 +38,9 @@ opt.splitright = true
 opt.splitbelow = true
 opt.autochdir = false
 opt.iskeyword:append("-")
-opt.mouse:append("a")                                   -- make mouse available
-opt.clipboard:append("unnamedplus")                     -- copy from clipboard
+opt.mouse:append("a") -- make mouse available
+opt.clipboard:append("unnamedplus") -- copy from clipboard
 opt.modifiable = true
 opt.encoding = "UTF-8"
+opt.updatetime = 100 -- faster writing of swapfile on disk (default 4000)
+opt.whichwrap = "<,>" -- enable cursor to go to beginning of next line from end of line before
