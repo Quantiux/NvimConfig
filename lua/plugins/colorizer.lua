26b3d29735567b1750a7b46380418eb1d2c1952d
Quantiux <roy@quantiux.com>
2024-07-02 08:29:15 -0400

    Added colorizer plugin

---
+++ lua/plugins/colorizer.lua
@@ -0,0 +1,25 @@
+local config = function()
+  require("colorizer").setup {
+    filetypes = { "*" },
+    user_default_options = {
+      RGB = true, -- #RGB hex codes
+      RRGGBB = true, -- #RRGGBB hex codes
+      names = true, -- "Name" codes like Blue oe blue
+      RRGGBBAA = true, -- #RRGGBBAA hex codes
+      AARRGGBB = true, -- 0xAARRGGBB hex codes
+      rgb_fn = true, -- CSS rgb() and rgba() functions
+      hsl_fn = true, -- CSS hsl() and hsla() functions
+      css = false, -- Enable all CSS features: rgb_fn, hsl_fn, names, RGB, RRGGBB
+      css_fn = false, -- Enable all CSS *functions*: rgb_fn, hsl_fn
+      mode = "background", -- Set the display mode.)
+      virtualtext = "■",
+    },
+    buftypes = {},
+  }
+end
+
+return {
+	"NvChad/nvim-colorizer.lua",
+	event = { "BufReadPre", "BufNewFile" },
+	config = config,
+}
