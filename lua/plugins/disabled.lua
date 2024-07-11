d4aa8588f1d3e09521eb1ddc70ec2764dd65744a
Quantiux <roy@quantiux.com>
2024-07-10 20:13:32 -0400

    Disabled obsidian-bridge.nvim plugin

--- lua/plugins/disabled.lua
+++ lua/plugins/disabled.lua
@@ -3,4 +3,5 @@ return {
 	{ "WhoIsSethDaniel/mason-tool-installer.nvim", enabled = false },
 	{ "stevearc/oil.nvim", enabled = false },
 	{ "iamcco/markdown-preview.nvim", enabled = false },
+	{ "oflisback/obsidian-bridge.nvim", enabled = false },
 }
