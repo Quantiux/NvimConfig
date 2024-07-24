bd5b57e9ee4c7324ff6e54c788d79a6b5227fcd4
Quantiux <roy@quantiux.com>
2024-07-23 21:34:18 -0400

    Added which-key mapping for commenting

--- lua/plugins/comment.lua
+++ lua/plugins/comment.lua
@@ -1,5 +1,16 @@
+local config = function()
+	-- which-key mapping
+	local wk = require("which-key")
+	wk.register({
+		["<leader>/"] = { "<Plug>(comment_toggle_linewise_current)", "Toggle comment (normal)" },
+	}, { mode = "n" }) -- normal mode mapping
+	wk.register({
+		["<leader>/"] = { "<Plug>(comment_toggle_linewise_visual)", "Toggle comment (visual)" },
+	}, { mode = "v" }) -- visual mode mapping
+end
+
 return {
-  "numToStr/Comment.nvim",
-  opts = {},
-  lazy = false,
+	"numToStr/Comment.nvim",
+	config = config,
+	lazy = false,
 }
