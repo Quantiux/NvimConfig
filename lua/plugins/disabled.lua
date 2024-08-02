e9507d8dd96fcf4e0b2e96453762b0099c407a8d
Quantiux <roy@quantiux.com>
2024-08-02 08:27:56 -0400

    Disabled plugins Nvim-R and cmp-nvim-r

--- lua/plugins/disabled.lua
+++ lua/plugins/disabled.lua
@@ -2,6 +2,7 @@
 return {
 	{ "WhoIsSethDaniel/mason-tool-installer.nvim", enabled = false },
 	{ "stevearc/oil.nvim", enabled = false },
-	{ "iamcco/markdown-preview.nvim", enabled = false },
 	{ "oflisback/obsidian-bridge.nvim", enabled = false },
+	{ "jalvesaq/Nvim-R", enabled = false },
+	{ "jalvesaq/cmp-nvim-r", enabled = false },
 }
