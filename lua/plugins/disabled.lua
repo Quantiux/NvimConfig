0cdc32e3264c1fc6c3d4374ad1dfdd452bf61bae
Quantiux <roy@quantiux.com>
2024-08-02 13:43:25 -0400

    Minor edit

--- lua/plugins/disabled.lua
+++ lua/plugins/disabled.lua
@@ -1,8 +1,8 @@
--- add plugin here to disable
+-- Add plugin here to disable -> Reload Neovim
 return {
 	{ "WhoIsSethDaniel/mason-tool-installer.nvim", enabled = false },
 	{ "stevearc/oil.nvim", enabled = false },
 	{ "oflisback/obsidian-bridge.nvim", enabled = false },
+	-- { "R-nvim/R.nvim", enabled = false }, -- temporarily disabled
 	{ "jalvesaq/Nvim-R", enabled = false },
-	{ "jalvesaq/cmp-nvim-r", enabled = false },
 }
