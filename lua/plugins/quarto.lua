3cf190640dc3b8298b7ddb4c03c8402e21ca1d81
Quantiux <roy@quantiux.com>
2024-08-24 00:16:30 -0400

    Added Quarto plugin

---
+++ lua/plugins/quarto.lua
@@ -0,0 +1,36 @@
+local config = function()
+	local quarto = require("quarto")
+	quarto.setup({
+		debug = false,
+		closePreviewOnExit = true,
+		lspFeatures = {
+			enabled = true,
+			chunks = "curly",
+			languages = { "r", "python" },
+			-- languages = { "r", "python", "julia", "bash", "html" },
+			diagnostics = {
+				enabled = true,
+				triggers = { "BufWritePost" },
+			},
+			completion = {
+				enabled = true,
+			},
+		},
+		codeRunner = {
+			enabled = false,
+			default_method = nil, -- 'molten' or 'slime'
+			ft_runners = {}, -- filetype to runner, ie. `{ python = "molten" }`.
+			-- Takes precedence over `default_method`
+			never_run = { "yaml" }, -- filetypes which are never sent to a code runner
+		},
+	})
+end
+
+return {
+	"quarto-dev/quarto-nvim",
+  ft = { "quarto" },
+	dependencies = {
+		"jmbuhr/otter.nvim",
+	},
+	config = config,
+}
