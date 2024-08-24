d20c8f10745b3aed2e5935f0ab10ff924980f8ea
Quantiux <roy@quantiux.com>
2024-08-24 00:17:55 -0400

    Minor edits

--- lua/plugins/disabled.lua
+++ lua/plugins/disabled.lua
@@ -1,7 +1,6 @@
 -- Add plugin here to disable -> Reload Neovim
 return {
-	{ "stevearc/oil.nvim", enabled = false },
 	{ "oflisback/obsidian-bridge.nvim", enabled = false },
-	{ "jalvesaq/Nvim-R", enabled = false },
-	{ "R-nvim/R.nvim", enabled = false }, -- temporarily disabled
+	{ "quarto-dev/quarto-nvim", enabled = false },
+	{ "jmbuhr/otter.nvim", enabled = false },
 }
