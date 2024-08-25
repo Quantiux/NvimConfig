6feebe2e8f6e8fd848f942037feae393a9991cf8
Quantiux <roy@quantiux.com>
2024-08-25 16:23:47 -0400

    Replaced config function by opts table

--- lua/plugins/image.lua
+++ lua/plugins/image.lua
@@ -1,32 +1,4 @@
-local config = function()
-	require("image").setup({
-		backend = "kitty",
-		integrations = {
-			markdown = {
-				enabled = true,
-				clear_in_insert_mode = false,
-				download_remote_images = true,
-				only_render_image_at_cursor = false,
-				filetypes = { "markdown", "vimwiki" }, -- markdown extensions (ie. quarto) can go here
-			},
-			html = { -- make sure treesitter html parser is installed
-				enabled = false,
-			},
-			css = { -- make sure treesitter css parser is installed
-				enabled = false,
-			},
-		},
-		max_width = nil,
-		max_height = nil,
-		max_width_window_percentage = nil,
-		max_height_window_percentage = 30,
-		window_overlap_clear_enabled = false, -- toggles images when windows are overlapped
-		window_overlap_clear_ft_ignore = { "cmp_menu", "cmp_docs", "" },
-		editor_only_render_when_focused = false,
-		tmux_show_only_in_active_window = false,
-		hijack_file_patterns = { "*.png", "*.jpg", "*.jpeg", "*.gif", "*.webp", "*.avif" },
-	})
-end
+-- necessary package prerequisites: ImageMagick, LuaJIT, magick (see Obsidian note on image rendering with Neovim)
 
 -- path to load magick (~/.luarocks/share/lua/5.1/magick/init.lua)
 package.path = package.path
@@ -35,8 +7,40 @@ package.path = package.path
 	.. "/.luarocks/share/lua/5.1/?/init.lua"
 package.path = package.path .. ";" .. vim.fn.expand("$HOME") .. "/.luarocks/share/lua/5.1/?.lua"
 
+local opts = {
+	backend = "kitty",
+	integrations = {
+		markdown = {
+			enabled = true,
+			clear_in_insert_mode = false,
+			download_remote_images = true,
+			only_render_image_at_cursor = true,
+			filetypes = { "markdown", "vimwiki" }, -- markdown extensions (ie. quarto) can go here
+		},
+		neorg = {
+			enabled = false,
+		},
+		html = { -- make sure treesitter html parser is installed
+			enabled = false,
+		},
+		css = { -- make sure treesitter css parser is installed
+			enabled = false,
+		},
+	},
+	-- next 5 lines from https://github.com/benlubas/molten-nvim/blob/main/docs/Not-So-Quick-Start-Guide.md#after-imagenvim-is-working
+	max_width = 120,
+	max_height = 12,
+	max_width_window_percentage = math.huge,
+	max_height_window_percentage = math.huge,
+	window_overlap_clear_enabled = true, -- toggles images when windows are overlapped
+	window_overlap_clear_ft_ignore = { "cmp_menu", "cmp_docs", "" },
+	editor_only_render_when_focused = false,
+	tmux_show_only_in_active_window = false,
+	hijack_file_patterns = { "*.png", "*.jpg", "*.jpeg", "*.gif", "*.webp", "*.avif" },
+}
+
 return {
 	"3rd/image.nvim",
 	event = "BufReadPre",
-	config = config,
+	opts = opts,
 }
