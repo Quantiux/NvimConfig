0b6e134698cc128689465534f5c4f7be2e543c48
Quantiux <roy@quantiux.com>
2024-08-25 21:37:38 -0400

    Enabled quarto plugins

--- lua/plugins/disabled.lua
+++ lua/plugins/disabled.lua
@@ -1,8 +1,6 @@
 -- Add plugin here to disable -> Reload Neovim
 return {
 	{ "oflisback/obsidian-bridge.nvim", enabled = false },
-	{ "quarto-dev/quarto-nvim", enabled = false },
-	{ "jmbuhr/otter.nvim", enabled = false },
 	{ "CRAG666/code_runner.nvim", enabled = false },
 	{ "CRAG666/betterTerm.nvim", enabled = false },
 }
